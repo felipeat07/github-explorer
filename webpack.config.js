f3b8db8093c7b4a586bfa1b33c8e7f07cf55593a
Felipe Tavares <felipeat07@gmail.com>
2022-05-11 22:02:10 +0100

    trocando as componentes para tsx

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     
     devtool: isDevelopment ? 'eval-source-map': 'source-map',
 
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,7 +18,7 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
 
     devServer: {
@@ -38,7 +38,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude:/node_modules/,
                 use: {
                     loader: 'babel-loader',
